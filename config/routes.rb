4fae72886e5ee6ab43fc856d1e392b1dacc399aa
tsyan <anna.tsykalova@gmail.com>
2014-02-03 18:58:10 -0500

    changed route structure to have routes nested inside journeys

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 BetterCommute::Application.routes.draw do
-  resources :routes
-
-  resources :journeys
 
   resources :users
+
+  resources :journeys do
+    resources :routes
+  end
+
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
