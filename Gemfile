afd51e3800eef91a712146949feb33a2749934b4
tsyan <anna.tsykalova@gmail.com>
2014-02-03 17:02:23 -0500

    initial commit - created rails app, edited gemfile and database.yml

---
+++ Gemfile
@@ -0,0 +1,27 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.0.2'
+gem 'pg'
+gem 'httparty'
+
+group :development do
+	gem 'pry'
+	gem 'pry-nav'
+	gem 'pry-stack_explorer'
+	gem 'annotate'
+	gem 'quiet_assets'
+	gem 'better_errors'
+	gem 'binding_of_caller'
+end
+
+gem 'sass-rails', '~> 4.0.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'jquery-rails'
+gem 'jbuilder', '~> 1.2'
+
+group :doc do
+  # bundle exec rake doc:rails generates the API under doc/api.
+  gem 'sdoc', require: false
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BetterCommute::Application.config.secret_key_base = '04b3d4127afee1c1ddc4740343c67acb261c18c5379d983b9fe9656b7caa6ffa5839fff7c0ab58294756c0a9dbe4268e9e1df0955af15daf694a8f49f6ad3e48'
