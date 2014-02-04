b5bd5eddf43360ccd72bbd1aedfc76f425618eef
tsyan <anna.tsykalova@gmail.com>
2014-02-03 20:39:21 -0500

    creating, saving, and viewing new journeys works

--- app/controllers/journeys_controller.rb
+++ app/controllers/journeys_controller.rb
@@ -1,25 +1,31 @@
 class JourneysController < ApplicationController
 
+	def show
+		@journey = Journey.find(params[:id])
+	end
+
 	def new
 		@journey = Journey.new
 	end
 
 	def create
-		@user = User.new # later this should be an if-else-end statement, if user is logged in then set @user as user
+		# @user = User.new("Test") # later this should be an if-else-end statement, if user is logged in then set @user as user
 		@journey = Journey.new(journey_params)
 
 		if @journey.save
-  		redirect_to journey_path
+  		redirect_to journey_path(@journey)
   	else
   		render :new
   	end
 
 	end
 
+
+
 	private
 
 	def journey_params
-		params.require(:journey).permit(:name, :user_id, :origin, :destination, :time_must_arrive_by)
+		params.require(:journey).permit(:name, :user_id, :origin, :destination, :time_must_arrive_by, :day_of_travel)
 	end
 
 end
