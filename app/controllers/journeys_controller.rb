a563f3b74f0243d2c0c2196189f6417e98e01ef4
tsyan <anna.tsykalova@gmail.com>
2014-02-03 19:45:17 -0500

    made a form for creating a new journey

--- app/controllers/journeys_controller.rb
+++ app/controllers/journeys_controller.rb
@@ -1,2 +1,25 @@
 class JourneysController < ApplicationController
+
+	def new
+		@journey = Journey.new
+	end
+
+	def create
+		@user = User.new # later this should be an if-else-end statement, if user is logged in then set @user as user
+		@journey = Journey.new(journey_params)
+
+		if @journey.save
+  		redirect_to journey_path
+  	else
+  		render :new
+  	end
+
+	end
+
+	private
+
+	def journey_params
+		params.require(:journey).permit(:name, :user_id, :origin, :destination, :time_must_arrive_by)
+	end
+
 end
