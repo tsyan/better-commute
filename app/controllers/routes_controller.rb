977daa0a5258661a6c7edb11cd71ab0e66916056
tsyan <anna.tsykalova@gmail.com>
2014-02-17 23:35:11 -0500

    explicitly ordered routes by chronological order

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -2,14 +2,7 @@ class RoutesController < ApplicationController
 
 	def index
 		@journey = Journey.find(params[:journey_id])
-		@route_1 = @journey.routes[0]
-		@route_2 = @journey.routes[1]
-		@route_3 = @journey.routes[2]
-		@route_4 = @journey.routes[3]
-		@route_5 = @journey.routes[4]
-		@route_6 = @journey.routes[5]
-		@route_7 = @journey.routes[6]
-		@route_8 = @journey.routes[7]
+		@routes = @journey.routes.order(departure_time: :asc)
 	end
 
 end
