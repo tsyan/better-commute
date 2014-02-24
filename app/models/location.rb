06f55fc2588e37fe45cb067223a12c5f88d3ad2b
tsyan <anna.tsykalova@gmail.com>
2014-02-24 00:36:06 -0500

    made variable names more consistent

--- app/models/location.rb
+++ app/models/location.rb
@@ -13,17 +13,17 @@ class Location
 
 	def get_address
 		search_url = "http://maps.googleapis.com/maps/api/geocode/json?address=#{@user_input}&bounds=42.215297,-71.350708|42.548022,-70.883789&sensor=false"
-		@origin_geocode = Rails.cache.fetch(["origin geocode", search_url], expires_in: 1.week) do
+		@geocode = Rails.cache.fetch(["origin geocode", search_url], expires_in: 1.week) do
 			HTTParty.get(URI.encode(search_url))["results"][0]
 		end
-		return if @origin_geocode.blank?
-		formatted_address = @origin_geocode["formatted_address"].chomp!(", USA")
+		return if @geocode.blank?
+		formatted_address = @geocode["formatted_address"].chomp!(", USA")
 	end
 
 	def get_coordinates
-		return if @origin_geocode.blank?
-		lat = @origin_geocode["geometry"]["location"]["lat"].to_s
-		lon = @origin_geocode["geometry"]["location"]["lng"].to_s
+		return if @geocode.blank?
+		lat = @geocode["geometry"]["location"]["lat"].to_s
+		lon = @geocode["geometry"]["location"]["lng"].to_s
 		origin_coordinates = lat + "," + lon
 	end
 
