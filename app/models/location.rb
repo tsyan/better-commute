fff5ae7e784ecbbe3ab089e9138f197f3c50eddd
tsyan <anna.tsykalova@gmail.com>
2014-02-24 01:06:15 -0500

    getting geocode is now a private method

--- app/models/location.rb
+++ app/models/location.rb
@@ -5,17 +5,22 @@ class Location
 
 	def initialize(user_input)
 		@user_input = user_input
+		@geocode = get_geocode # private method
 		@address = get_address # private method
 		@coordinates = get_coordinates # private method
 	end
 
 	private
 
-	def get_address
+	def get_geocode
 		search_url = "http://maps.googleapis.com/maps/api/geocode/json?address=#{@user_input}&bounds=42.215297,-71.350708|42.548022,-70.883789&sensor=false"
 		@geocode = Rails.cache.fetch(["origin geocode", search_url], expires_in: 1.week) do
 			HTTParty.get(URI.encode(search_url))["results"][0]
 		end
+		return if @geocode.blank?
+	end
+
+	def get_address
 		return if @geocode.blank?
 		formatted_address = @geocode["formatted_address"].chomp!(", USA")
 	end
