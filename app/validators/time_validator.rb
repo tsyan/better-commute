2c2e3b8a388808652e48a77ce464e24dcb9db6fe
Anna Tsykalova <tsyan@users.noreply.github.com>
2014-03-19 18:54:55 -0400

    departure or arrival times can be 30 minutes in the past

--- app/validators/time_validator.rb
+++ app/validators/time_validator.rb
@@ -4,7 +4,8 @@ class TimeValidator < ActiveModel::EachValidator
     if value - Time.zone.now < -86400 || value - Time.zone.now > 31536000
       record.errors.add(:base, "Time must be within the next year.")
     # if user enters 4pm when it's already 5pm, add one day and continue
-    elsif value - Time.zone.now < 0 && value - Time.zone.now > -86400
+    # but entering a time up to 30 minutes ago is okay
+    elsif value - Time.zone.now < -3601 && value - Time.zone.now > -86400
       record.update(attribute => value + 86400)
     end
   end
